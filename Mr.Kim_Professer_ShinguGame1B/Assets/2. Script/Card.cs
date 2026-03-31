28450ebab1f689999477709daf121b64342d4bb2
Dev_horang2.H <hawengtiger@users.noreply.github.com>
2026-03-31 18:31:05 +0900

    [Feat] 03-31일분

--- Mr.Kim_Professer_ShinguGame1B/Assets/2. Script/Card.cs
+++ Mr.Kim_Professer_ShinguGame1B/Assets/2. Script/Card.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// ===| 카드 스크립트 |===
+/// </summary>
 public class Card : MonoBehaviour
 {
+    /// <summary>
+    /// | public | ===========================
+    /// </summary>
+    
     public TextMeshProUGUI numText;
     public int cardNumber;
     public float rotationSpeed;
+    public bool isClick = false;
+
+    /// <summary>
+    /// | private | =========================
+    /// </summary>
+    private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
+    private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
+
 
     //변수
     //정보
@@ -29,15 +44,29 @@ public class Card : MonoBehaviour
 
         //==AND==OR==
         //==&&===||==
-        if(transform.eulerAngles.y >= 0 && transform.eulerAngles.y < 180)
+        if(isClick)    //만약 각도가 0보다 같거나 크거나 180보다 작고 isClick이 ture일 경우 실행됨.
         {
-            transform.Rotate(0, rotationSpeed, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotationSpeed * Time.deltaTime);
         }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        //Time.deltaTime 드랍되는 프레임을 곱해줌으로 써 일정한 프레임을 유지하기 위한 코드.
+    }
+
+    /// <summary>
+    /// ===| 카드를 클릭했을 때 |===
+    /// </summary>
+    public void ClickCard()
+    {
+        isClick = !isClick;     //클릭 유무확인 bool변수
     }
 
     //카드 돌리기.
 
     //카드
 
-
+    //카드가 뒤집혔을때 유니티 특성상 뒷면은 적용이 안되게되는데. (Ignore Reversed Graphics를 비활성화)
 }
