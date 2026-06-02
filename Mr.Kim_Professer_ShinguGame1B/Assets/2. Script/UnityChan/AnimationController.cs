0add66387e718d5672e5991a175504dc9b99a11b
Dev_horang2.H <hawengtiger@users.noreply.github.com>
2026-06-02 18:40:00 +0900

    [Feat] 6월2일분

---
+++ Mr.Kim_Professer_ShinguGame1B/Assets/2. Script/UnityChan/AnimationController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using KinematicCharacterController;
+using KinematicCharacterController.Examples;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator animator;
+    public ExampleCharacterController character;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", character.Motor.Velocity.magnitude / 10);
+    }
+}
